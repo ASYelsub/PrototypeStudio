99519bd7eae88f099f16ca1b3e1a7973efd57a10
Abigail <asyelsub@gmail.com>
2021-11-07 20:10:04 -0500

    box set up and fixed a shader

--- Assets/TwoAnimations/Scripts/TreeCameraController.cs
+++ Assets/TwoAnimations/Scripts/TreeCameraController.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 public class TreeCameraController : MonoBehaviour
 {
-    float x;
-    float y;
-    float z;
     float yaw;
     float pitch;
-    float roll;       
 
     [Header("Settings")]
     public float mouseSensitivity = 5f;
-    public float moveSpeed = 2f;
     public float colliderSize = 5f;
 
     [Header("Other Objects")]
@@ -22,22 +17,18 @@ public class TreeCameraController : MonoBehaviour
     }
     void Update(){
 
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity; // yaw input
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity; // pitch input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
         pitch -= mouseY;
         yaw += mouseX;
         transform.localRotation = Quaternion.Euler(pitch, yaw, 0.0f);
-        //myCollider.transform.localRotation = Quaternion.Euler(pitch, yaw, 0.0f);
-
 
         Vector2 inputDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-       // inputDirection.Normalize();
         Move(inputDirection);
     }
     bool col = false;
     public void RecieveCollision(){
         col = true;
-        //Debug.Log("here");
     }
     public void LeaveCollision(){
         col = false;
