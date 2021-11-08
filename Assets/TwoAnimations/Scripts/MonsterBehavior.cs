83ec96ece51570be971eb6279243608ea6ed1302
Abigail <asyelsub@gmail.com>
2021-11-08 10:41:49 -0500

    sound stuff

--- Assets/TwoAnimations/Scripts/MonsterBehavior.cs
+++ Assets/TwoAnimations/Scripts/MonsterBehavior.cs
@@ -14,6 +14,10 @@ public class MonsterBehavior : MonoBehaviour
     public GameObject body;
     public GameObject entire;
     public GameObject target;
+    public GameObject lLengthf;
+    public GameObject rLengthf;
+    public GameObject lLengthb;
+    public GameObject rLengthb;
 
     enum RotState {down, goingUp, up, goingForward, atForward, goingDown};
 
@@ -23,51 +27,73 @@ public class MonsterBehavior : MonoBehaviour
     float rsf = Mathf.PI;
 
     [Header("Adjustment")]
-    public float legHeight = 1f;
-    public float legForward = 1f;
+    public float legHeight;
+    public float legForward;
+    public float rotAngle;
+    public float moveSpeed;
+    public float moveInterval;
+    public float restTime;
     
     Vector3 rinit;
     Vector3 linit;
     void Start(){
         rinit = rs.transform.localPosition;
         linit = ls.transform.localPosition;
+        startLength = lLengthb.transform.localScale;
+        backLengthStartPos = lLengthb.transform.localPosition;
+        frontLengthStartPos = lLengthf.transform.localPosition; 
+
     }
     void FixedUpdate(){
+       // TargetRot();
+        TargetRotBasic();
+        LegSpinnerMovement();
+        LegJointRot();
+      //  LegLengthMovement();
+    }
+    float moveTimer = 0f;
+    void TargetRotBasic(){
+        target.transform.Rotate(0, -.01f, 0);
+    }
+    void TargetRot(){
+        if(moveTimer > moveInterval && moveTimer <moveInterval+restTime){
+            moveTimer += Time.deltaTime*moveSpeed;
+        }else if (moveTimer > moveInterval+restTime){
+            moveTimer = 0;
+        }else{
+            moveTimer += Time.deltaTime;
+            target.transform.Rotate(0, -.1f, 0);
+        }
+
+    }
+    void LegSpinnerMovement(){
         lsf += Time.fixedDeltaTime;
         rsf += Time.fixedDeltaTime;
-       ls.transform.localPosition = linit + new Vector3(Mathf.Cos(lsf)*legForward,
-                                                Mathf.Sin(lsf)*legHeight,
-                                                0);
-        rs.transform.localPosition = rinit +new Vector3(Mathf.Cos(rsf)*legForward,
-                                                 Mathf.Sin(rsf)*legHeight, 
+        ls.transform.localPosition = linit + new Vector3(Mathf.Cos(lsf) * legForward,
+                                                 Mathf.Sin(lsf) * legHeight,
                                                  0);
-        target.transform.Rotate(0, -.1f, 0);
+        rs.transform.localPosition = rinit + new Vector3(Mathf.Cos(rsf) * legForward,
+                                                 Mathf.Sin(rsf) * legHeight,
+                                                 0);
     }
-    bool rotating = false;
-    bool moving = false;
-    IEnumerator MoveLeg(Transform legspinner, Vector3 start, Vector3 end){
-        moving = true;
-        float t = 0f;
-        while (t < 1){
-            legspinner.transform.localPosition = Vector3.Lerp(start,end,t);
-            t+= Time.fixedDeltaTime;
-            yield return null;
-        }
-        moving = false;
-        yield return null;
+    void LegJointRot(){
+        lb.transform.Rotate(new Vector3(0,Mathf.Cos(lsf)*rotAngle,0));
+        lf.transform.Rotate(new Vector3(0, Mathf.Cos(lsf) * rotAngle, 0));
+        rb.transform.Rotate(new Vector3(0, Mathf.Cos(rsf) * rotAngle, 0));
+        rf.transform.Rotate(new Vector3(0, Mathf.Cos(rsf) * rotAngle, 0));
     }
-    IEnumerator RotateLegJoint(Transform legspinner, Transform frontLeg, Transform backLeg, Vector3 addVec, float length){
-        rotating = true;
-        float t = 0f;
-        while(t<length){
-            legspinner.Rotate(addVec);
-            frontLeg.Rotate(-addVec);
-            backLeg.Rotate(-addVec);
-            t += Time.fixedDeltaTime;
-            yield return null;
-        }
-        rotating = false;
-        yield return null;
-    }
+    Vector3 startLength;
+    Vector3 backLengthStartPos;
+    Vector3 frontLengthStartPos;
 
+    void LegLengthMovement(){
+        lLengthb.transform.localScale = startLength + new Vector3(0,0,Mathf.Cos(lsf));
+        rLengthb.transform.localScale = startLength + new Vector3(0, 0,Mathf.Cos(rsf));
+        rLengthf.transform.localScale = startLength + new Vector3(0, 0, Mathf.Cos(rsf));
+        lLengthf.transform.localScale = startLength + new Vector3(0, 0, Mathf.Cos(lsf));
+        lLengthb.transform.localPosition = backLengthStartPos + new Vector3(0, 0, Mathf.Cos(rsf));
+        rLengthb.transform.localPosition = backLengthStartPos + new Vector3(0, 0, Mathf.Cos(lsf));
+        lLengthf.transform.localPosition = frontLengthStartPos + new Vector3(0, 0, Mathf.Cos(lsf));
+        rLengthf.transform.localPosition = frontLengthStartPos + new Vector3(0, 0, Mathf.Cos(rsf));
+    }
 }
