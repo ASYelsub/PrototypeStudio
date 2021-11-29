0329caa66630473ee6cc6bc7edd5c1bb4b037214
Abigail <asyelsub@gmail.com>
2021-11-29 12:22:47 -0500

    attempt to fix lfs stuff

--- Assets/Portrait/RetrogradeGameBehavior.cs
+++ Assets/Portrait/RetrogradeGameBehavior.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class RetrogradeGameBehavior : MonoBehaviour
 {
     public List<Frame> storyFrames = new List<Frame>();
-
+    public Material imageMat;
     [System.Serializable]
     public struct Frame
     {
@@ -20,27 +21,47 @@ public class RetrogradeGameBehavior : MonoBehaviour
 
     
 
-    
+    public GameObject startPanel;
+    public GameObject gamePanel;
     public Text textBox;
     public Image spriteImage;
     public int currentFrame = 0;
     public bool typing = false;
+    public AudioSource clickAS;
+    public AudioSource typeAS;
+    public AudioClip clickSound;
     string currentString = "";
     [Range(5,13)]
     public float spriteScale = 10f;
     public float typeSpeed = 0.1f;
-
+    static int GameState = 0;
     void Start(){
-        NextFrame();
+       startPanel.SetActive(true);
+       gamePanel.SetActive(false);
     }
     
     public void Update(){
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Return)){
+            if(GameState==0){
+                startPanel.SetActive(false);
+                gamePanel.SetActive(true);
+                GameState = 1;
+            }
             NextFrame();
         }
+         if(Input.GetKeyDown(KeyCode.Mouse1)){
+             SpeedTextUp();
+         }
+        if(Input.GetKeyDown(KeyCode.R)){
+            Restart();
+        }
+    }
+    void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void NextFrame(){
         if(!typing){
+            clickAS.PlayOneShot(clickSound);
             SetVisuals(storyFrames[currentFrame].photo);
             SetTextTypeWrite(storyFrames[currentFrame].text);
             if(currentFrame<storyFrames.Count-1){
@@ -49,7 +70,20 @@ public class RetrogradeGameBehavior : MonoBehaviour
             return;
         } 
     }
+    float originalSpeed;
+    bool textSped = false;
+    void SpeedTextUp(){
+        if(!typing)
+            return;
+        if(textSped)
+            return;
+        
+        originalSpeed = typeSpeed;
+        typeSpeed *= .2f;
+        textSped = true;
+        return;
 
+    }
     //Visual//
     void SetVisuals(Sprite sprite)
     {
@@ -59,6 +93,23 @@ public class RetrogradeGameBehavior : MonoBehaviour
     void SetImage(Sprite sprite)
     {
         spriteImage.sprite = sprite;
+       // imageMat.SetTexture("_MainTex",textureFromSprite(sprite));
+    }
+    public static Texture2D textureFromSprite(Sprite sprite)
+    {
+        if (sprite.rect.width != sprite.texture.width)
+        {
+            Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+            Color[] newColors = sprite.texture.GetPixels((int)sprite.textureRect.x,
+                                                         (int)sprite.textureRect.y,
+                                                         (int)sprite.textureRect.width,
+                                                         (int)sprite.textureRect.height);
+            newText.SetPixels(newColors);
+            newText.Apply();
+            return newText;
+        }
+        else
+            return sprite.texture;
     }
 
     void SetImageSize(Vector2 spriteSize)
@@ -69,7 +120,7 @@ public class RetrogradeGameBehavior : MonoBehaviour
     {
         float width = (float)sprite.bounds.size.x;
         float height = (float)sprite.bounds.size.y;
-        Debug.Log(width + height);
+      //  Debug.Log(width + height);
         return new Vector2(width, height);
     }
 
@@ -85,25 +136,26 @@ public class RetrogradeGameBehavior : MonoBehaviour
         currentString = newText;
         StartCoroutine(TypeWrite(newText, textBox));
     }
+    public List<AudioClip> typeSounds = new List<AudioClip>();
     IEnumerator TypeWrite(string typeString, Text placeToType)
     {
         typing = true;
         foreach (char c in typeString)
         {
             placeToType.text += c;
-            // if (!System.Char.IsWhiteSpace(c))
-            // {
-            //     if (soundFlip)
-            //         audioSource.PlayOneShot(typeBeep[0]);
-            //     else
-            //         audioSource.PlayOneShot(typeBeep[1]);
-            //     if (System.Char.IsPunctuation(c))
-            //         soundFlip = !soundFlip;
-
-            // }
+             if (!System.Char.IsWhiteSpace(c))
+             {  if(System.Char.IsPunctuation(c)){
+                 typeAS.PlayOneShot(clickSound);
+                }else{
+                    int f = Random.Range(0, typeSounds.Count);
+                    typeAS.PlayOneShot(typeSounds[f]);
+                }
+            }
             yield return new WaitForSeconds(typeSpeed);
         }
         typing = false;
+        typeSpeed = originalSpeed;
+        textSped = false;
         yield return null;
     }
 
